a9466aa6e6f69f40c41c64b0d31d492fa831147b
udaybegyall <udaybegyall12345@gmail.com>
2022-01-02 23:34:40 +0530

    badges-component

---
+++ src/components/badges/timetaken.tsx
@@ -0,0 +1,14 @@
+interface Props {
+
+}
+
+const Timetaken = (props: Props) => {
+    return (
+        <div className='time-taken-badge'>
+            <h3>Time :</h3>
+            <h4>30 Sec</h4>
+        </div>
+    )
+}
+
+export default Timetaken;
