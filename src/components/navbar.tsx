8ff03f64995b4587cc50ec8a43d433980a8d8d2c
udaybegyall <udaybegyall12345@gmail.com>
2022-01-03 20:52:17 +0530

    navbar-update

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,14 @@
+import logo from '../assets/Vector.png';
+
 const Navbar = () => {
-    return <nav className="navbar" />
+    return (
+        <nav className="navbar" >
+            <div className="logo">
+                <img src={logo} alt="logo" />
+                <h3>SORTING VISULIZER.</h3>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar;
